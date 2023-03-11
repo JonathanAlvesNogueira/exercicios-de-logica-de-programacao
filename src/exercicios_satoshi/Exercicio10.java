be756e313c7fa863deeaa95773c7b736d00c0c19
Jonathan <joth10@live.com>
2023-03-11 14:37:32 -0300

    Aplicando mais exercicios

---
+++ src/exercicios_satoshi/Exercicio10.java
@@ -0,0 +1,27 @@
+package exercicios_satoshi;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número:");
+        double num1 = scan.nextDouble();
+
+        System.out.println("Informe o segundo número:");
+        double num2 = scan.nextDouble();
+
+        double diferenca = num1 - num2;
+
+        System.out.println("A diferença entre os números é: " + diferenca);
+		
+		
+		
+		
+		
+	}
+
+}
